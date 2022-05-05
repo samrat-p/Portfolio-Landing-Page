e3bc1b88c90212b35cc162285617b406a7e4223c
Samrat Mitra <mitra.stark.68@gmail.com>
2022-05-06 02:06:02 +0530

    Add files via upload

---
+++ frame 3.jsx
@@ -0,0 +1,46 @@
+function App() {
+    return (
+      <div className="page-1">
+        <div className="flex-row">
+          <div className="flex-col">
+            <div className="sd">
+              SD
+            </div>
+            <img className="component-1" src="component-1-1.svg" />
+            <img className="component-1-1" src="component-1-2.svg" />
+            <div className="follow-me">
+              Follow Me
+            </div>
+          </div>
+          <div className="flex-col-1">
+            <div className="overlap-group">
+              <div className="stock-app">
+                Stock App
+              </div>
+              <h1 className="title">
+                Groww
+              </h1>
+            </div>
+            <div className="with-grow-investing">
+              With Grow, investing in mutual funds, fixed deposits, indian stocks, US stocks, IPO &amp; trading F&amp;O made easy, fast and
+              secured. Enjoy 100% paperless onboarding without any hasless and invest in popular mutual funds, stocks, FD &amp; IPOs in a few
+              mintues 🚀 from the comfort of your home.
+            </div>
+            <div className="flex-row-1">
+              <div className="view-projects">
+                VIEW PROJECTS
+              </div>
+              <img className="component-1-2" src="component-1.svg" />
+            </div>
+          </div>
+        </div>
+        <div className="flex-row-2">
+          <img className="entypo-socialgithub-with-circle" src="entypo-social-github-with-circle.svg" />
+          <img className="icon-instagram" src="instagram.svg" />
+          <img className="icon-linkedin" src="linkedin-in-1.svg" />
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
